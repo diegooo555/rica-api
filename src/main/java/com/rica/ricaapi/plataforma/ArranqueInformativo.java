47885b6813353a76df89b5cfc569d78e85b091ae
diegooo555 <dieguito555alf@outlook.es>
2026-09-15 17:57:38 -0500

    Plataforma  Folder

---
+++ src/main/java/com/rica/ricaapi/plataforma/ArranqueInformativo.java
@@ -0,0 +1,20 @@
+package com.rica.ricaapi.plataforma;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ArranqueInformativo implements CommandLineRunner {
+
+    private final SaludoInstitucionalService saludoInstitucionalService;
+
+    public ArranqueInformativo(SaludoInstitucionalService saludoInstitucionalService) {
+        this.saludoInstitucionalService = saludoInstitucionalService;
+    }
+
+    @Override
+    public void run(String... args) {
+        System.out.println(saludoInstitucionalService.mensajeDeBienvenida());
+    }
+
+}
---
+++ src/main/java/com/rica/ricaapi/plataforma/SaludoInstitucionalService.java
@@ -0,0 +1,12 @@
+package com.rica.ricaapi.plataforma;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SaludoInstitucionalService {
+
+    public String mensajeDeBienvenida() {
+        return "RICA está en línea - Red de Investigación y Colaboración Académica.";
+    }
+
+}
