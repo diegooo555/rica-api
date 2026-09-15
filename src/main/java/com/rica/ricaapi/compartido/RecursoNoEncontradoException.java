8ac4740f743c7e4105b4ff657f070b21b7a506cb
diegooo555 <dieguito555alf@outlook.es>
2026-09-15 17:57:09 -0500

    Compartido  Folder

---
+++ src/main/java/com/rica/ricaapi/compartido/RecursoNoEncontradoException.java
@@ -0,0 +1,9 @@
+package com.rica.ricaapi.compartido;
+
+public class RecursoNoEncontradoException extends RuntimeException {
+
+    public RecursoNoEncontradoException(String mensaje) {
+        super(mensaje);
+    }
+
+}
