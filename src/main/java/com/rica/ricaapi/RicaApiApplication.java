b6d1c1f1eca912dc48d1e2356fadd921a7dcdc17
diegooo555 <dieguito555alf@outlook.es>
2026-09-15 17:56:01 -0500

    Bean SpringBoot Application

---
+++ src/main/java/com/rica/ricaapi/RicaApiApplication.java
@@ -0,0 +1,13 @@
+package com.rica.ricaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RicaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RicaApiApplication.class, args);
+	}
+
+}
